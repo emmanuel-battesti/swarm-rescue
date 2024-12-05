1590c64437ac4b584c9bee0b678d559d62e48768
Romain Planchon <romain.planchon@telecom-paris.fr>
2024-12-05 15:27:13 +0100

    gitignore add the logs

--- src/swarm_rescue/solutions/my_dronewalletienne.py
+++ src/swarm_rescue/solutions/my_dronewalletienne.py
@@ -5,7 +5,7 @@ Le drone suit les murs
 from enum import Enum
 import math
 from typing import Optional
-
+import cv2
 import numpy as np
 
 from spg_overlay.entities.drone_abstract import DroneAbstract
@@ -32,22 +32,29 @@ class MyDroneWall(DroneAbstract):
         # Initialisation de l'activité
         self.state  = self.Activity.SEARCHING_WALL
         
-        
         # Paramètre following walls
-        self.dmax = 50 # distance max pour suivre un mur
-        self.dist_to_stay = 30 # distance à laquelle on veut rester du mur
-        
-        self.Kp = 4/math.pi # correction proportionnelle # theoriquement j'aurais du mettre 2
-        self.Kd = 1 # correction dérivée
-        self.Kdist = 1 # correction proportionnelle à la distance
+        self.dmax = 70 # distance max pour suivre un mur
+        self.dist_to_stay = 50 # distance à laquelle on veut rester du mur
         self.speed_following_wall = 0.2
-        self.Ki = (1/10) * 1/20 # correction intégrale
+
+        self.Kp_angle = 4/math.pi # correction proportionnelle # theoriquement j'aurais du mettre 2
+        self.Kd_angle = 1 # correction dérivée
+        self.Ki_angle = (1/10) * 1/20 # correction intégrale
         self.past_ten_errors_angle = [0]*10
         
         self.Kp_distance = 1/(abs(self.dmax-self.dist_to_stay))
-        self.Ki_distance = 1/abs(self.dist_to_stay-self.dmax) * 1/10 * 1/20
+        self.Ki_distance = 1/abs(self.dist_to_stay-self.dmax) * 1/20
+        self.Kd_distance = 1
         self.past_ten_errors_distance = [0]*10
 
+        # paramètres affichage
+        self.record_log = False
+        self.log_file = "log.txt"
+        self.log = {"epsilon_wall_angle":[],"epsilon_wall_distance":[]} # buffer de taille 50 pour les logs
+        self.flush_interval = 50  # Number of timesteps before flushing buffer
+        self.timestep_count = 0  # Counter to track timesteps        
+
+        
     def define_message_for_all(self):
         """
         Here, we don't need communication...
@@ -75,19 +82,32 @@ class MyDroneWall(DroneAbstract):
         
         command = {"forward": self.speed_following_wall,"lateral": 0.0,"rotation": 0.0,"grasper": 0}
 
-        if found_wall :
+        if self.state is self.Activity.FOLLOWING_WALL:
 
             epsilon_wall_angle = normalize_angle(epsilon_wall_angle) 
-            
+            self.log["epsilon_wall_angle"].append(epsilon_wall_angle)
+
+            self.timestep_count += 1
+
+            # Periodically flush the buffer to the log file
+            if self.timestep_count % self.flush_interval == 0:
+                
+                
+                with open(self.log_file, "a") as log_file:
+                    for angle in self.log["epsilon_wall_angle"]:
+                        log_file.write(f"{angle}\n")
+                print("Buffer flushed to log file")
+                self.log["epsilon_wall_angle"].clear()  # Clear the buffer
+
             self.past_ten_errors_angle.pop(0)
             self.past_ten_errors_angle.append(epsilon_wall_angle)
             
             deriv_epsilon_wall_angle = normalize_angle(self.odometer_values()[2]) # vitesse angulaire
 
-            correction_proportionnelle = self.Kp * epsilon_wall_angle
-            correction_derivee = self.Kd * deriv_epsilon_wall_angle
+            correction_proportionnelle = self.Kp_angle * epsilon_wall_angle
+            correction_derivee = self.Kd_angle * deriv_epsilon_wall_angle
             #correction_sharp_turns = self.Kdist*1/(1+np.exp(-0.1*(min_dist-self.dmax)))
-            correction_integrale = self.Ki * sum(self.past_ten_errors_angle)
+            correction_integrale = self.Ki_distance * sum(self.past_ten_errors_angle)
 
             rotation = correction_proportionnelle + correction_derivee + correction_integrale 
             rotation = min( max(-1,rotation) , 1 ) 
@@ -100,7 +120,8 @@ class MyDroneWall(DroneAbstract):
             command["rotation"] = rotation
 
             # lateral control to stay at a distance from the wall
-            epsilon_wall_distance = + min_dist - self.dist_to_stay 
+            epsilon_wall_distance =  min_dist - self.dist_to_stay 
+            self.log["epsilon_wall_distance"].append(epsilon_wall_distance)
             deriv_epsilon_wall_distance = - np.sin(self.odometer_values()[1])*self.odometer_values()[0] # vitesse latérale
             
             print(f"deriv_epsilon_wall_distance : {deriv_epsilon_wall_distance}")
@@ -108,13 +129,13 @@ class MyDroneWall(DroneAbstract):
             self.past_ten_errors_distance.append(epsilon_wall_distance)
 
             correction_proportionnelle_distance = self.Kp_distance * epsilon_wall_distance
-            correction_derivee_distance = self.Kd * deriv_epsilon_wall_distance
+            correction_derivee_distance = self.Kd_distance * deriv_epsilon_wall_distance
             correction_integrale_distance = self.Ki_distance * sum(self.past_ten_errors_distance)
 
             lateral = correction_proportionnelle_distance + correction_derivee_distance + correction_integrale_distance
             lateral = min( max(-1,lateral) , 1 )
 
-            activation_distance = 0.1
+            activation_distance = 0.05
             if abs(lateral) < activation_distance:
                 lateral = 0.0
 
@@ -130,7 +151,7 @@ class MyDroneWall(DroneAbstract):
         """
         -> ( bool near_obstacle , float epsilon_wall_angle )
         where epsilon_wall_angle is the (counter-clockwise convention) angle made
-        between the drone and the nearest wall
+        between the drone and the nearest wall - pi/2
         """
         lidar_values = self_lidar.get_sensor_values()
 
